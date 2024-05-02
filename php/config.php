5f5ea85630309771aec4ef22f8dc17c21ea91f4a
Shimaa Essam <125494212+shimaa123456@users.noreply.github.com>
2024-05-02 18:10:02 +0300

    Delete php directory

--- php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Here change variables with your own settings
-$mysqlhost='localhost';
-$mysqlusername='root';
-$mysqlpassword=''; 
-$mysqldb='userscheck';
-$conn=mysqli_connect($mysqlhost,$mysqlusername,$mysqlpassword,$mysqldb);
-
-?>
